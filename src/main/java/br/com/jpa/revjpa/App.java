853c5841b8c5e937e067b01907182c945929bde6
Joabe Guimaraes Querino Kachorroski <Joabe.Kachorroski@wsci.com.br>
2017-06-02 15:41:30 -0400

    Comitando

---
+++ src/main/java/br/com/jpa/revjpa/App.java
@@ -0,0 +1,13 @@
+package br.com.jpa.revjpa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
