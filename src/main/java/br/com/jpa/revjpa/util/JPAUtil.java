fb9f6da7f2f6289689d4cb35ee5c685084747d7c
Joabe Guimaraes Querino Kachorroski <Joabe.Kachorroski@wsci.com.br>
2017-06-02 17:33:54 -0400

    update

---
+++ src/main/java/br/com/jpa/revjpa/util/JPAUtil.java
@@ -0,0 +1,25 @@
+package br.com.jpa.revjpa.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+	
+	private EntityManagerFactory factory;
+	private static JPAUtil instance;
+	
+	private JPAUtil(){
+		this.factory = Persistence.createEntityManagerFactory("REVJPA");
+	}
+	public static synchronized JPAUtil getInstance(){
+		if (instance == null) {
+			instance = new JPAUtil();
+		}
+		return instance;
+	}
+	
+	public EntityManager geEntityManager(){
+		return factory.createEntityManager();
+	}
+}
